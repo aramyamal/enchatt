559e058370e4451d24743fc4cd517b50d48b9e12
aramyamal <aram@aramjamal.com>
2025-02-04 15:33:31 +0100

    added service layer
    Co-authored-by: jonathanstörm <jonathannstorm@users.noreply.github.com>
    Co-authored-by: Mouqla <Mouqla@users.noreply.github.com>

--- server/src/model/message.interface.ts
+++ server/src/model/message.interface.ts
@@ -1,5 +1,5 @@
 export interface Message {
     sender: string;
-    time: Date;
+    time: number;
     content: string;
 }
---
+++ server/src/service/chatService.ts
@@ -0,0 +1,30 @@
+import { Chat } from "../model/chat.interface";
+import { Message } from "../model/message.interface";
+
+export class chatService {
+    private chats: Map<string, Chat> = new Map(); //replace with database
+
+    async createOrGetChat(key: string): Promise<Chat> {
+        if (this.chats.has(key) === undefined) {
+            const newChat: Chat = { messages: [] };
+            this.chats.set(key, newChat);
+            return JSON.parse(JSON.stringify(this.chats.get(key))); //get deep copy
+        }
+        return JSON.parse(JSON.stringify(this.chats.get(key)));
+    }
+
+    async sendMessage(key: string, sender: string, content: string): Promise<boolean> {
+        const message: Message = {
+            sender: sender,
+            time: Date.now(),
+            content: content
+        };
+
+        const chat: Chat | undefined = this.chats.get(key);
+        if (!chat) {
+            return false;
+        }
+        chat.messages.push(message);
+        return true;
+    }
+}
