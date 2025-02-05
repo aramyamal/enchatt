72455389d196211238bd7ca016075db4be678168
aramyamal <aram@aramjamal.com>
2025-02-05 18:20:11 +0100

    updated getOrCreateChat() to use error handling and modern deep copy

--- server/src/service/chatService.ts
+++ server/src/service/chatService.ts
@@ -4,13 +4,20 @@ import { Message } from "../model/message.interface";
 export class ChatService {
     private chats: Map<string, Chat> = new Map(); //replace with database
 
+    private getChat(key: string): Chat {
+        const chat = this.chats.get(key);
+        if (!chat) {
+           throw new Error(`Chat with key ${key} not found.`);
+        }
+        return chat;
+    }
+
     async createOrGetChat(key: string): Promise<Chat> {
         if (!this.chats.has(key)) {
             const newChat: Chat = { messages: [] };
             this.chats.set(key, newChat);
-            return JSON.parse(JSON.stringify(this.chats.get(key))); //get deep copy
         }
-        return JSON.parse(JSON.stringify(this.chats.get(key)));
+        return structuredClone(this.getChat(key));
     }
 
     async sendMessage(key: string, sender: string, content: string): Promise<boolean> {
