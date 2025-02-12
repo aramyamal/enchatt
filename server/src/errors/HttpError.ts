29f2313b6c290e069c8ae963fde9418195816a8f
aramyamal <aram@aramjamal.com>
2025-02-12 21:53:30 +0100

    feat(errors): add HttpError class with statusCode

---
+++ server/src/errors/HttpError.ts
@@ -0,0 +1,11 @@
+export class HttpError extends Error {
+    public readonly statusCode: number;
+
+    constructor(statusCode: number, message: string) {
+        super(message);
+        this.statusCode = statusCode;
+        this.name = "HttpError";
+
+        Object.setPrototypeOf(this, HttpError.prototype);
+    }
+}
