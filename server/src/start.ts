e858ea0982cc0e96fb6bc07e4933ff10dea4e045
aramyamal <aram@aramjamal.com>
2025-02-17 14:12:22 +0100

    chore(client): setup project structure and depends
    Co-authored-by: jonathanstörm <jonathannstorm@users.noreply.github.com>

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { chatRouter } from "./router/chat";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/", chatRouter);
 
