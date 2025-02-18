0c143e0083b1657fe679c4b715db9f2e101aa3d9
aramyamal <aram@aramjamal.com>
2025-02-18 13:59:11 +0100

    fix(server): only allow cors from frontend testing
    Co-authored-by: jonathanstörm <jonathannstorm@users.noreply.github.com>

--- server/src/start.ts
+++ server/src/start.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 export const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow credentials (cookies, auth headers)
+}));
 app.use("/", chatRouter);
 
