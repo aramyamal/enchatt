1ac423cf52a2090073237e0e2d53cbe875f54109
Mouqla <103849615+Mouqla@users.noreply.github.com>
2025-02-24 00:01:59 +0100

    test for chatSubmit modified

--- client/src/tests/chatSubmit.test.tsx
+++ client/src/tests/chatSubmit.test.tsx
@@ -6,15 +6,19 @@ jest.mock('../api', ()=> ({
     createMessage: jest.fn(),
 }));
 
-test('Should send a message with correct key when enter is pressed, assuming that the selected key is key1', async() => {
-    const {getByPlaceholderText } = render(<ChatSubmit onKeyChange={()=>{}}/>);
-    const inputField = getByPlaceholderText('Enter message for Key 1...');
-    
-    fireEvent.change(inputField, {target: {value: 'Hi friends'}});
-    
-    fireEvent.keyDown(inputField, {key:'Enter', code: 'Enter'});
-    
-    await waitFor(() => {
-        expect(createMessage).toHaveBeenCalledWith('browser_user', 'Hi friends', '');
+describe('ChatSubmit', () => {
+    it('Should send a message with correct key when enter is pressed, assuming that the selected key is key1', async() => {
+        const {getByPlaceholderText } = render(<ChatSubmit onKeyChange={()=>{}}/>);
+        const inputField = getByPlaceholderText('Enter message for Key 1...');
+
+        fireEvent.change(inputField, {target: {value: 'Hi friends'}});
+
+        fireEvent.keyDown(inputField, {key:'Enter', code: 'Enter'});
+
+        await waitFor(() => {
+            expect(createMessage).toHaveBeenCalledWith('browser_user', 'Hi friends', '');
+        });
     });
 });
+
+    
