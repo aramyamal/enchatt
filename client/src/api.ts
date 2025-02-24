9dc69edc812a1538e59bd7fdca392aac62ade4eb
Mouqla <103849615+Mouqla@users.noreply.github.com>
2025-02-24 13:29:53 +0100

    api and Message modified

--- client/src/api.ts
+++ client/src/api.ts
@@ -6,24 +6,27 @@ export type Chat = {
     messages: Message[];
 }
 
+export type KeyString = "Key 1" | "Key 2" | "Key 3" | "Key 4" | undefined; 
+
 export type Message = {
-    sender: string;
-    time: number;
-    content: string;
+    sender: string,
+    time: number,
+    content: string,
+    key: KeyString
 }
 
 // TODO: change for finished product
 const BASE_URL = "http://localhost:8080";
 
 export async function getMultipleChats(keys: string[]): Promise<Chat> {
-    const chatPromises = axios.get<Chat>(`${BASE_URL}/chats/` , {
-            params : {
-                key1 : keys[0],
-                key2 : keys[1],
-                key3 : keys[2],
-                key4 : keys[3]
-            }
-        }).then(res => res.data);
+    const chatPromises = axios.get<Chat>(`${BASE_URL}/chats/`, {
+        params: {
+            key1: keys[0],
+            key2: keys[1],
+            key3: keys[2],
+            key4: keys[3]
+        }
+    }).then(res => res.data);
 
     return await chatPromises;
 }
--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -1,10 +1,27 @@
-import { Message } from "../../api";
+import { Message, KeyString } from "../../api";
+import classes from "./Message.module.css";
 
 export function MessageComponent({ message }: { message: Message }) {
+
+    function getKeyClass(keyString: KeyString) {
+        switch (keyString) {
+            case "Key 1":
+                return classes.key1;
+            case "Key 2":
+                return classes.key2;
+            case "Key 3":
+                return classes.key3;
+            case "Key 4":
+                return classes.key4;
+            default:
+                return;
+        }
+    }
+
     return (
         <>
             <div className="my-2">
-                <span className="font-serif">{message.sender}:</span> {message.content}
+                <span className={`font-serif ${getKeyClass(message.key)}`}>{message.sender}:</span> {message.content}
             </div>
         </>
     );
