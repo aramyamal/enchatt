b7e022b69b968f70d33934728cb04e6301ec98b9
aramyamal <aram@aramjamal.com>
2025-03-14 11:20:47 +0100

    docs(keys): add docs for key utils

--- client/src/utils/keys.ts
+++ client/src/utils/keys.ts
@@ -1,11 +1,21 @@
+/**
+ * Represents a string type containig key names for interfacing.
+ */
 export type KeyString = "Key 1" | "Key 2" | "Key 3" | "Key 4";
 
+/**
+ * Represents a raw key object containing the original key string, its hashed 
+ * version, and an optional cryptographic salt.
+ */
 export interface RawKeyObject {
     raw: string,
     hashed: string,
     salt?: string
 }
 
+/**
+ * Represents the 4 raw keys the user may input, where each key is optional.
+ */
 export interface RawKeys {
     key1?: RawKeyObject
     key2?: RawKeyObject
@@ -13,6 +23,9 @@ export interface RawKeys {
     key4?: RawKeyObject
 }
 
+/**
+ * Represents the 4 derived keys the user may use to encrypt/decrypt messages.
+ */
 export interface DerivedKeys {
     key1?: CryptoKey;
     key2?: CryptoKey;
@@ -20,13 +33,14 @@ export interface DerivedKeys {
     key4?: CryptoKey;
 }
 
-export interface HashedKeys {
-    key1?: string;
-    key2?: string;
-    key3?: string;
-    key4?: string;
-}
-
+/**
+ * Converts a key string (e.g., "Key 1") into its corresponding 
+ * object property key (e.g., "key1").
+ *
+ * @param key - The key string to convert.
+ * @returns The corresponding property key in the `RawKeys` type.
+ * @throws Will throw an error if the provided key is not recognized.
+ */
 export function convertToKeyString(key: string): keyof RawKeys {
     switch (key) {
         case "Key 1": return "key1";
@@ -37,6 +51,12 @@ export function convertToKeyString(key: string): keyof RawKeys {
     }
 };
 
+/**
+ * Retrieves the CSS class name associated with a given `KeyString` value.
+ *
+ * @param keyString - The key string to map to a class name.
+ * @returns The corresponding CSS class name.
+ */
 export function getKeyClass(keyString: KeyString): string {
     switch (keyString) {
         case "Key 1":
